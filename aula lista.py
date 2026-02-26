84db62311123a7c22a5814bef3237c2071cb5a90
pedrofalchi-fullstack <peeh.falchi@gmail.com>
2026-02-26 18:05:25 -0300

    Add files via upload

---
+++ aula lista.py
@@ -0,0 +1,18 @@
+sla = ["nome 1", "nome 2", 1, 2.123 , 3]
+
+
+
+for i in range(0, 4 , 1):
+    print(sla[i])
+
+    
+numeros = []
+
+for i in range(0, 5, 1):
+    x = int(input('menssagem sla: '))
+    numeros.append(x)
+
+
+
+for num in numeros:
+    print(num)
